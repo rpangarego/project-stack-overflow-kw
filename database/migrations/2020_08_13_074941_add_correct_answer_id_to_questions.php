610f0a5cd1760ddeacdf6c6d89e488c4a4b39254
Stefan Adisurya <stefanadisurya@gmail.com>
2020-08-13 17:57:56 +0700

    update migration

--- database/migrations/2020_08_13_074941_add_correct_answer_id_to_questions.php
+++ database/migrations/2020_08_13_074941_add_correct_answer_id_to_questions.php
@@ -14,7 +14,7 @@ class AddCorrectAnswerIdToQuestions extends Migration
     public function up()
     {
         Schema::table('questions', function (Blueprint $table) {
-            $table->unsignedBigInteger('correct_answer_id');
+            $table->unsignedBigInteger('correct_answer_id')->nullable();
             $table->foreign('correct_answer_id')->references('answer_id')->on('answers');
         });
     }
