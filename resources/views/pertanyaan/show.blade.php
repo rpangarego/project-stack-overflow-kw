3f5d71527b82d2bbbc96f98966f179d6fe6ac720
rpangarego <rpangarego@gmail.com>
2020-08-14 10:16:09 +0700

    tambah form jawaban di detail pertanyaan

--- resources/views/pertanyaan/show.blade.php
+++ resources/views/pertanyaan/show.blade.php
@@ -4,8 +4,11 @@
 <title>Stack Overflow</title>
 @endsection
 
-@section('content')
+@push('script-head')
+<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+@endpush
 
+@section('content')
 <div class="row">
     <div class="col">
         <!-- Dropdown Card Example -->
@@ -34,12 +37,95 @@
             <!-- Card Body -->
             <div class="card-body">
                 <p>Deskripsi: {!!$question->content!!}</p>
-                <hr>
                 <div class="text-muted" style="font-size: 14px">Ditanyakan oleh: {{$question->user['name']}}</div>
-                <br>
+                <hr>
+                <div>
+                    <div class="buttons float-right">
+                        {{-- upvote button --}}
+                        <a href="#" class="btn btn-light btn-icon-split btn-sm mx-1">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-arrow-up"></i>
+                            </span>
+                            <span class="text">Upvote</span>
+                        </a>
+                        {{-- downvote button --}}
+                        <a href="#" class="btn btn-light btn-icon-split btn-sm mx-1">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-arrow-down"></i>
+                            </span>
+                            <span class="text">Downvote</span>
+                        </a>
+                        {{-- comment button --}}
+                        <a href="#" class="btn btn-light btn-icon-split btn-sm mx-1">
+                            <span class="icon text-white-50">
+                                <i class="far fa-comment"></i>
+                            </span>
+                            <span class="text">Komentar</span>
+                        </a>
+                    </div>
+                </div>
                 <a href="/pertanyaan" class="d-block"> &larr; Kembali</a>
+
+                <hr>
+                {{-- form jawaban --}}
+                <h6 class="m-0 font-weight-bold text-primary">
+                    <div class="text-gray-700">Jawaban:</div>
+                </h6>
+                <form action="#" method="POST">
+                    @csrf
+                    <input type="hidden" value="{{$question->question_id}}" name="question_id">
+
+                    <div class="form-group">
+                        {{-- <label for="content" class="text-grey-700">Jawaban</label> --}}
+
+                        <textarea name="content" id="isi"
+                            class="form-control my-editor">{!! old('content', $content ?? '') !!}</textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit Jawaban</button>
+                </form>
             </div>
+
         </div>
     </div>
 </div>
 @endsection
+
+
+@push('scripts')
+<script>
+    var editor_config = {
+      path_absolute : "/",
+      selector: "textarea.my-editor",
+      plugins: [
+        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+        "searchreplace wordcount visualblocks visualchars code fullscreen",
+        "insertdatetime media nonbreaking save table contextmenu directionality",
+        "emoticons template paste textcolor colorpicker textpattern"
+      ],
+      toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+      relative_urls: false,
+      file_browser_callback : function(field_name, url, type, win) {
+        var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+        var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+        var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
+        if (type == 'image') {
+          cmsURL = cmsURL + "&type=Images";
+        } else {
+          cmsURL = cmsURL + "&type=Files";
+        }
+
+        tinyMCE.activeEditor.windowManager.open({
+          file : cmsURL,
+          title : 'Filemanager',
+          width : x * 0.8,
+          height : y * 0.8,
+          resizable : "yes",
+          close_previous : "no"
+        });
+      }
+    };
+
+    tinymce.init(editor_config);
+</script>
+@endpush
