e939f6995ff3845c0796636a4ccbcb120595f519
Stefan Adisurya <stefanadisurya@gmail.com>
2020-08-14 14:42:18 +0700

    fix conflict

--- app/Answer.php
+++ app/Answer.php
@@ -6,12 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-<<<<<<< HEAD
     public function comments_answers() {
         return $this->hasMany('App\CommentAnswer', 'comment_id');
-=======
+    }
+    
     public function user() {
         return $this->belongsTo('App\User', 'user_id');
->>>>>>> 324bfbfcd27d9dde661537b95e8af2fc747e33c6
     }
 }
