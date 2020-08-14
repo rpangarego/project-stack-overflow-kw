40532d9dea2ce5dd18ae99e4a4dafb7560e2d7c3
Stefan Adisurya <stefanadisurya@gmail.com>
2020-08-14 14:30:06 +0700

    delete comment on show.blade.php

--- resources/views/pertanyaan/show.blade.php
+++ resources/views/pertanyaan/show.blade.php
@@ -66,32 +66,7 @@
                 </div>
                 <a href="/pertanyaan" class="d-block"> &larr; Kembali</a>
                 <hr>
-                {{-- <div class="row">
-    <div class="col-6">
-        <h1 class="mt-3">Detail Pertanyaan</h1>
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">{{$question->title}}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">{!!$question->content!!}</h6>
-                <p class="card-text">Ditanyakan pada: {{$question->created_at}}</p>
-                <p class="card-text">Diperbarui pada: {{$question->updated_at}}</p>
-                <p class="card-text">Ditanyakan oleh: {{$question->user['name']}}</p>
-                <div class="buttons">
-                    <a href="/pertanyaan" class="btn btn-secondary">Back</a>
-                    <a href="{{$question->question_id}}/edit" class="btn btn-primary">Ubah</a>
-                    <form action="/pertanyaan/{{$question->question_id}}" method="post" class="d-inline">
-                        @method('delete')
-                        @csrf
-                        <button type="submit" class="btn btn-danger">Hapus</button>
-                    </form>
-                </div>
-                <br>
-                <div class="d-flex justify-content-around">
-                    <a href="#"><i class="fas fa-arrow-up"></i></a>
-                    <a href="#"><i class="fas fa-arrow-down"></i></a>
-                    <a href="#"><i class="far fa-comment-dots"></i></a>
-                </div>
-                <hr>
+
                 {{-- form jawaban --}}
                 <h6 class="m-0 font-weight-bold text-primary">
                     <div class="text-gray-700">Jawaban:</div>
@@ -101,7 +76,6 @@
                     <div class="card-body">
                         <h5 class="card-title">{{$answer->user['name']}}</h5>
                         <p class="card-text">{!!$answer->content!!}</p>
-                        {{-- <input type="text" value="/jawaban/{{$answer->answer_id}}"> --}}
                         <form action="/jawaban/{{$answer->answer_id}}" method="post" class="d-inline">
                             @csrf
                             @method('delete')
@@ -116,7 +90,6 @@
                     <input type="hidden" value="{{$question->question_id}}" name="question_id">
 
                     <div class="form-group">
-                        {{-- <label for="content" class="text-grey-700">Jawaban</label> --}}
 
                         <textarea name="content" id="isi"
                             class="form-control my-editor">{!! old('content', $content ?? '') !!}</textarea>
